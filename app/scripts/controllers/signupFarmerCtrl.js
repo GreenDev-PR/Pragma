07fe89032e0ef4921595b62d2895d30bd2c7f862
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2014-03-03 21:59:46 -0400

    Signup views

---
+++ app/scripts/controllers/signupFarmerCtrl.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('pragmaApp').controller('signupFarmerCtrl',['$scope', function($scope){
+
+	$scope.map = {
+		center:{
+			latitude: 18.229351,
+			longitude: -66.453767
+		},
+		zoom: 8,
+
+	};
+
+}]);
