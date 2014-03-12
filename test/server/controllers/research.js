20d9e27e805de6f9e62e388739b2af014f3e452a
Victor J. Reventos <victor.reventos1@gmail.com>
2014-03-12 16:12:48 -0400

    Changing from /research to /research/variables

--- test/server/controllers/research.js
+++ test/server/controllers/research.js
@@ -6,12 +6,29 @@ seed = require('../seedData'),
 seedData = seed.data,
 app = require('../../../server');
 
+var endpoint = '/api/research/variables/';
+
 describe('Research controller', function () {
   describe('getVariables', function () {
     it('should be able to bring all the variables', function (done) {
-      request(app).get('/api/research').expect(200)
+      request(app).get(endpoint).expect(200)
       .end(helper.isBodyEqual(seedData.goesVariables, done));
     });
+  });
+
+  describe.skip('getDataForVariable', function() {
+    it('should bring the latest data');
+    it('should bring the latest data for a given latitude and longitude');
+    it('should bring the data for a specified time range');
+    it('should bring the data for a specified time range and location');
+    it('should be able to bring the data in csv format');
+  });
+
+  describe.skip('getMapForVariable', function() {
+    it('should bring the latest map data');
+    it('should bring the latest map data for a given latitude and longitude');
+    it('should bring the map data for a specified time range');
+    it('should bring the map data for a specified time range and location');
   });
 });
 
