2977d7234c083a4712b1cd8fdcda5864fb9ddec6
Victor J. Reventos <victor.reventos1@gmail.com>
2014-03-20 15:58:02 -0400

    Get a specific cropType

--- test/server/controllers/cropType.js
+++ test/server/controllers/cropType.js
@@ -12,4 +12,19 @@ describe('CropType controller', function() {
     .expect(200)
     .end(helper.isEqualWithoutIdAndTimestamps(seedData.cropTypes, done));
   });
+
+  describe('get a specific crop type', function() {
+    it('should bring a cropType with id 5', function(done) {
+      request(app).get('/api/cropTypes/5')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(helper.isEqualWithoutIdAndTimestamps(seedData.cropTypes[4], done));
+    });
+
+    it('should return a 404', function(done) {
+      request(app).get('/api/cropTypes/100000')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+    });
+  });
 });
