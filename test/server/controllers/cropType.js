22214a2a7f6795ce39e81ac9637cfadb1173a003
Victor J. Reventos <victor.reventos1@gmail.com>
2014-03-20 18:22:29 -0400

    Default crop data routes implementation

--- test/server/controllers/cropType.js
+++ test/server/controllers/cropType.js
@@ -26,5 +26,35 @@ describe('CropType controller', function() {
       .expect('Content-Type', /json/)
       .expect(404, done);
     });
+
+    describe('crop data', function() {
+      it('should get all the crop data', function(done) {
+        request(app).get('/api/cropTypes/1/cropData')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(helper.isEqualWithoutIdAndTimestamps([seedData.defaultCropData[0]], done));
+      });
+
+      it('should respond with a 404 for a crop data not in the db', function(done) {
+        request(app).get('/api/cropTypes/1000000/cropData')
+        .expect('Content-Type', /json/)
+        .expect(404, done);
+      });
+
+      describe('by id', function() {
+        it('should return a crop data with id 1', function(done) {
+          request(app).get('/api/cropTypes/1/cropData/1')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(helper.isEqualWithoutIdAndTimestamps(seedData.defaultCropData[0], done));
+        });
+
+        it('should return 404 for an id not in the db', function(done) {
+          request(app).get('/api/cropTypes/1/cropData/3')
+          .expect('Content-Type', /json/)
+          .expect(404, done);
+        });
+      });
+    });
   });
 });
