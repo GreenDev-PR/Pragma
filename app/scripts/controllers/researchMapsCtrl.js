5c2a292a8f11a7986fcf2c04056a46c0eec701ed
josediaz30 <jose.diaz30@upr.edu>
2014-03-31 18:09:27 -0400

    Updated date input boxes to use Angular UI bootstrap date picker.

--- app/scripts/controllers/researchMapsCtrl.js
+++ app/scripts/controllers/researchMapsCtrl.js
@@ -1,13 +1,46 @@
 'use strict';
 
 angular.module('pragmaApp')
-  .controller('ResearchMapsCtrl', ['$scope', 'variables', function ($scope, variables) {
+  .controller('ResearchMapsCtrl', ['$scope', '$filter','variables', function ($scope, $filter, variables) {
 
   var DEFAULT_INTERVAL = 3000;
 
+  $scope.startDate = {
+    value: new Date(),
+    opened: false,
+    open: function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.startDate.opened = true;
+    }
+  };
+
+  $scope.endDate = {
+    value: new Date(),
+    opened: false,
+    open: function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.endDate.opened = true;
+    }
+  };
+
+  $scope.showWeeks = false;
+
+  $scope.dateOptions = {
+    'date-format': '\'dd-MMMM-yyyy\'',
+    'starting-day': 1,
+    'datepicker-append-to-body': true,
+    'show-button-bar': false
+  };
+
+  $scope.minDate = '2009-01-01';
+  $scope.maxDate = '2014-04-27';
   //var MIN_INTERVAL = 1;
   //var MAX_INTERVAL = 10;
 
+  $scope.format = 'yyyy-MM-dd';
+  
   //$scope.slides = goesService.getSlides();
   //$scope.variables = goesService.getVariables();
 
@@ -16,13 +49,14 @@ angular.module('pragmaApp')
     $scope.variable =  $scope.variables[0].variableName;
   });
   
-  $scope.startDate = $scope.startDate;
-  $scope.endDate = $scope.endDate;
+  // $scope.startDate = $scope.startDate;
+  // $scope.endDate = $scope.endDate;
 
   $scope.state = {
     interval: DEFAULT_INTERVAL
   };
-  
+
+
  /* $scope.updateInterval = function () {
     if (MIN_INTERVAL <= $scope.newInterval && $scope.newInterval <= MAX_INTERVAL) {
       $scope.state.interval = $scope.newInterval * 1000;
@@ -33,10 +67,11 @@ angular.module('pragmaApp')
 
   $scope.generateSlideshow = function () {
     
-    console.log($scope.startDate);
-    console.log($scope.endDate);
+    var startDateWithoutTimeZone = $filter('date')($scope.startDate.value,$scope.format);
+    var endDateWithoutTimeZone = $filter('date')($scope.endDate.value,$scope.format);
 
-    variables.getMapsFor($scope.variable.variableName, $scope.startDate, $scope.endDate).then(function (result){
+    variables.getMapsFor($scope.variable.variableName, startDateWithoutTimeZone, endDateWithoutTimeZone)
+    .then(function (result){
       $scope.slides = result;
       console.log($scope.slides);
     });
