0078be80b5b56b2b36128b2f41d0cdc1aecd2828
Victor J. Reventos <victor.reventos1@gmail.com>
2014-03-31 21:21:27 -0400

    Allows the user to search its location.

--- app/scripts/controllers/signupFarmerCtrl.js
+++ app/scripts/controllers/signupFarmerCtrl.js
@@ -1,14 +1,37 @@
 'use strict';
 
-angular.module('pragmaApp').controller('signupFarmerCtrl',['$scope', function($scope){
+angular.module('pragmaApp').controller('signupFarmerCtrl',['$scope', 'Geocoder', function($scope, Geocoder){
 
 	$scope.map = {
 		center:{
 			latitude: 18.229351,
 			longitude: -66.453767
 		},
-		zoom: 8,
+    control: {},
+    zoom: 8
+  };
 
-	};
+  $scope.marker = {
+    coords: {
+      latitude: 18.229351,
+      longitude: -66.453767
+    },
+    options: {
+      draggable: true,
+      visible: true
+    }
+  };
 
+  $scope.user = {};
+  $scope.address = 'Bayamon, PR';
+
+  $scope.searchAddress = function() {
+    Geocoder.geocode( { 'address': $scope.address}).then(function(results) {
+      var location = results[0].geometry.location;
+      $scope.map.control.getGMap().setCenter(location);
+      $scope.map.zoom = 13;
+      $scope.marker.coords.latitude = location.lat();
+      $scope.marker.coords.longitude = location.lng();
+    });
+  };
 }]);
