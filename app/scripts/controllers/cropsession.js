7600f76abd10bcf2489eb515dd66646cd61f16a4
Victor J. Reventos <victor.reventos1@gmail.com>
2014-04-04 14:48:37 -0400

    Crop Session detail barebones.

--- app/scripts/controllers/cropsession.js
+++ app/scripts/controllers/cropsession.js
@@ -1,8 +1,6 @@
 'use strict';
 
 angular.module('pragmaApp')
-  .controller('CropsessionCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
+.controller('CropSessionCtrl', function ($scope) {
+  $scope.cropSession = {cropName: 'Platano Asesino'};
+});
--- app/scripts/controllers/projectPagesCtrl.js
+++ app/scripts/controllers/projectPagesCtrl.js
@@ -9,6 +9,7 @@ angular.module('pragmaApp').controller('ProjectPagesCtrl',function($scope) {
     {name: 'Dashboard', sref: 'dashboard.overview'},
     {name: 'ResearchMaps', sref: 'dashboard.researchMaps' },
     {name: 'ResearchVariables', sref: 'dashboard.researchVariables'},
-    {name: 'Plots Researcher', sref: 'dashboard.researchPlots'}
+    {name: 'Plots Researcher', sref: 'dashboard.researchPlots'},
+    {name: 'Crop Session Detail', sref: 'dashboard.viewCropSession({cropSessionId: 1})'}
   ];
 });
