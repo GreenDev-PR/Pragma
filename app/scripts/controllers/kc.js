e90481d658b8656de8b597f629289de3006be4b5
Victor J. Reventos <victor.reventos1@gmail.com>
2014-04-05 15:39:05 -0400

    Dynamic kc chart

--- app/scripts/controllers/kc.js
+++ app/scripts/controllers/kc.js
@@ -1,21 +1,66 @@
 'use strict';
 
 angular.module('pragmaApp')
-.controller('KcCtrl', function ($scope) {
+.controller('KcCtrl', function ($scope, cropSession) {
+
+  var generateSeries = function(cropSession) {
+    var initial = [0, cropSession.initialStageLength];
+    var dev = [initial[1], initial[1] + cropSession.developmentStageLength];
+    var mid = [dev[1], dev[1] + cropSession.midStageLength];
+    var late = [mid[1], mid[1] + cropSession.lateStageLength];
+    return [
+      {
+        name: 'Initial Stage',
+        data: [
+          [initial[0], cropSession.kcInitial],
+          [initial[1], cropSession.kcInitial]
+        ]
+      },
+      {
+        name: 'Development Stage',
+        data: [
+          [dev[0], cropSession.kcInitial],
+          [dev[1], cropSession.kcMid]
+        ]
+      },
+      {
+        name: 'Mid Stage',
+        data: [
+          [mid[0], cropSession.kcMid],
+          [mid[1], cropSession.kcMid]
+        ]
+      },
+      {
+        name: 'Late Stage',
+        data: [
+          [late[0], cropSession.kcMid],
+          [late[1], cropSession.kcEnd]
+        ]
+      },
+    ];
+  };
+
+  $scope.cropSession = cropSession;
+
   $scope.chartConfig = {
     options: {
       chart: {
-        type: 'line',
-        zoomType: 'x'
+        type: 'line'
       }
     },
-    series: [{
-      data: [10, 15, 12, 8, 7, 1, 1, 19, 15, 10]
-    }],
+    series: generateSeries(cropSession),
     title: {
       text: 'Crop Coefficient'
     },
-    xAxis: {currentMin: 0, currentMax: 10, minRange: 1},
     loading: false
   };
+
+  // $scope.update = function() {
+  //   $scope.chartConfig.series[0];
+  // };
+
+  $scope.save = function() {
+    $scope.chartConfig.series = generateSeries($scope.cropSession);
+  };
+
 });
