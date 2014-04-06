9dc7241803f840fc0052250562dc136a2f00491a
josediaz30 <jose.diaz30@upr.edu>
2014-04-06 14:21:49 -0400

    Added documentation for crop sessions, research maps and research variables.

--- app/scripts/controllers/cropsessions.js
+++ app/scripts/controllers/cropsessions.js
@@ -1,27 +1,68 @@
 'use strict';
-
+/**
+ * Crop Sessions Controller. 
+ * Provides appropriate functionalities to the crop sessions view. 
+ */
 angular.module('pragmaApp')
   .controller('CropSessionsCtrl',['$scope','$injector','CropSessions','CropTypes',
     function ($scope,$injector,CropSessions,CropTypes){
-	
+	/**
+   * Invoke the Crop session getAll method in order to collect all cropSessions of 
+   * the currently logged in user.
+   */
   CropSessions.getAll().then(function(cropSessions){
-		$scope.cropList = cropSessions;
+		
+    /**
+     * Define a scope cropList property with an crop session array containing the user's currently 
+     * ongoing crop sesssions.
+     * @type {{Object}}
+     */
+    $scope.cropList = cropSessions;
 
-    //Replace startDate with correct Date format
+    /**
+     * Modify cropList property to create a JavaScript Date object for the date string representation 
+     * property in each crop object in the cropList array.
+     * @param  {Object} entry A json object representaion of a single instance of a crop session 
+     */
     $scope.cropList.forEach(function(entry){
       entry.startDate = new Date(entry.startDate);
     });
+
 	});
 
+  /**
+   * Invoke the Crop Types getAll method inorder to collect all crop types supported by the system 
+   */
   CropTypes.getAll().then(function(types){
+
+    /**
+     * Define cropTypeList an array containing json object representation of each crop type
+     * supported by the system.
+     * @type {Object}
+     */
     $scope.cropTypeList = types;
+
   });
 
+  /** 
+   * Detete a crop session from the view. 
+   * @param  {Integer} index The location of the cropssion inside the crop sessions array.
+   */
   $scope.deleteCropSession = function(index){
+
+    /**
+     * CropList element at index location
+     * @type {Object}
+     */
     var temp = $scope.cropList[index];
+
     CropSessions.remove(temp.id).then(function(){
+
+      /* Remove elment at location index from the scope cropList property array*/
       $scope.cropList.splice(index,1);
+
     });
+
   };
   
 }]);
--- app/scripts/controllers/researchVariables.js
+++ app/scripts/controllers/researchVariables.js
@@ -1,8 +1,23 @@
 'use strict';
-
+/**
+ * Research Variables Controller. 
+ * Provides appropriate functionalities for the research variables views.
+ */
 angular.module('pragmaApp')
 .controller('ResearchVariablesCtrl', ['$scope','variables', function ($scope, variables) {
+	
+	/**
+	 * Invoke the variablesService getAll method to collect an json array representation of
+	 * the supported goes-variables
+	 */
 	variables.getAll().then(function(result){
+
+		/**
+		 * Define scope variables property with json object array containing all goes varaibles
+		 * supported by the system.
+		 * @type {Object|Array.<Object>}
+		 */
 		$scope.variables = result;
 	});
+
 }]);
