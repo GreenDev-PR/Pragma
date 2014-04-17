69630fc8e016bfd134d8d427dbd8dd0f1248b7ce
Victor J. Reventos <victor.reventos1@gmail.com>
2014-04-17 15:50:32 -0400

    Sharing data through object on the cropSession service

--- app/scripts/services/cropsessions.js
+++ app/scripts/services/cropsessions.js
@@ -6,12 +6,20 @@ angular.module('pragmaApp')
   var cropSessionEndpoint = 'users/me/cropSessions';
   var cropSessions = Restangular.all(cropSessionEndpoint);
 
+  var data = { cropSessions: [], irrigationEvents: [] };
+
   this.getAll = function() {
-    return cropSessions.getList();
+    return cropSessions.getList().then(function(cropSessions) {
+      data.cropSessions = cropSessions;
+      return data;
+    });
   };
 
   this.create = function(cropSession) {
-    return cropSessions.post(cropSession);
+    return cropSessions.post(cropSession).then(function(cropSession) {
+      data.cropSessions.push(cropSession);
+      return cropSession;
+    });
   };
 
   this.get = function(id) {
@@ -19,10 +27,31 @@ angular.module('pragmaApp')
   };
 
   this.remove = function(id) {
-    return Restangular.one(cropSessionEndpoint, id).remove();
+    return Restangular.one(cropSessionEndpoint, id).remove().then(function(removedCropSession) {
+      data.cropSessions = data.cropSessions.filter(function(cropSession) {
+        return cropSession.id !== id;
+      });
+
+      return removedCropSession;
+    });
   };
 
   this.calculateIrrigation = function(id, lastIrrigationDate) {
     return Restangular.one(cropSessionEndpoint, id).customGET('calculateIrrigation', {lastIrrigationDate: lastIrrigationDate});
+  };
+
+  this.getAllIrrigationEvents = function(cropSessionId) {
+    return Restangular.one(cropSessionEndpoint, cropSessionId).all('irrigationEvents').getList().then(function(irrigationEvents) {
+      data.irrigationEvents = irrigationEvents;
+      return data;
+    });
+  };
+
+  this.createIrrigationEvent = function(cropSessionId, irrigationEvent) {
+    return Restangular.one(cropSessionEndpoint, cropSessionId).all('irrigationEvents')
+        .post(irrigationEvent).then(function(createdIrrigationEvent) {
+          data.irrigationEvents.push(createdIrrigationEvent);
+          return createdIrrigationEvent;
+        });
   };
 });
