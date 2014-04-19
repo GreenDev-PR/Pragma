ec3426359bff37f97719e98af7b9902aa233eb50
Victor J. Reventos <victor.reventos1@gmail.com>
2014-04-19 16:29:28 -0400

    Using the resolvedVariables in ResearchVariablesCtrl

--- app/scripts/controllers/researchVariables.js
+++ app/scripts/controllers/researchVariables.js
@@ -1,26 +1,19 @@
 'use strict';
 /**
- * Research Variables Controller. 
- * 
+ * Research Variables Controller.
+ *
  * Provides appropriate functionalities for the research variables views.
- * This controller loads the all goes-variables supported by the system for 
+ * This controller loads the all goes-variables supported by the system for
  * research variables view.
  */
 angular.module('pragmaApp')
-.controller('ResearchVariablesCtrl', ['$scope','variables', function ($scope, variables) {
-	
+.controller('ResearchVariablesCtrl', function ($scope, resolvedVariables) {
+
 	/**
-	 * Invoke the variablesService getAll method to collect an json array representation of
-	 * the supported goes-variables
+	 * Define scope variables property with json object array containing all goes varaibles
+	 * supported by the system.
+	 * @type {Object|Array.<Object>}
 	 */
-	variables.getAll().then(function(result){
+	$scope.variables = resolvedVariables;
 
-		/**
-		 * Define scope variables property with json object array containing all goes varaibles
-		 * supported by the system.
-		 * @type {Object|Array.<Object>}
-		 */
-		$scope.variables = result;
-	});
-
-}]);
+});
